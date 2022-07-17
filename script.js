7f24513b6198a05768fae7d078e64133f712d92f
Elias Mendes <eliaxem69@gmail.com>
2022-07-17 00:43:51 -0300

    finalização do menu acima

---
+++ script.js
@@ -0,0 +1,32 @@
+var menu = document.querySelector('.menu')
+var header = document.querySelector('.header')
+var navIcons = document.querySelectorAll('.nav-icons')
+var navFooter = document.querySelector('.nav-footer')
+var navSpan = document.querySelectorAll('.nav-span.opacited')
+var logoMenu = document.querySelector('.logo')
+
+menu.addEventListener('click', () => {
+    header.classList.contains('desactived') ? (
+        menu.style.justifyContent = 'space-between',
+        header.style.width = '280px',
+        navIcons.forEach(element => {
+            element.style.width = '96%'}),
+        navFooter.style.paddingRight = '15px',
+        navSpan.forEach(element => {
+            element.style.opacity = '1'}),
+        logoMenu.style.display = 'block',
+        header.classList.remove('desactived'),
+        header.classList.add('actived')
+    ) : (
+        menu.style.justifyContent = 'flex-end',
+        header.style.width = '70px',
+        navIcons.forEach(element => {
+            element.style.width = '85%'}),
+        navFooter.style.paddingRight = '25px',
+        navSpan.forEach(element => {
+            element.style.opacity = '0'}),
+        logoMenu.style.display = 'none',
+        header.classList.remove('actived'),
+        header.classList.add('desactived')
+    )
+})
